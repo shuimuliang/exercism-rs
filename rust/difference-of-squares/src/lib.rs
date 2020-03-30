f67c0a7e693add0546f81bda46df6d126519d48f
shuimuliang@gmail.com <shuimuliang@gmail.com>
2020-03-30 18:03:15 +0800

    difference-of-squares

---
+++ rust/difference-of-squares/src/lib.rs
@@ -0,0 +1,29 @@
+/*
+pub fn square_of_sum(n: u32) -> u32 {
+    let mut sum = 0;
+    for i in 1..=n {
+        sum += i
+    }
+
+    sum * sum
+}
+
+pub fn sum_of_squares(n: u32) -> u32 {
+    let mut sum = 0;
+    for i in 1..=n {
+        sum += i*i
+    }
+    sum
+}
+*/
+pub fn square_of_sum(n: u32) -> u32 {
+    let sum: u32 = (1..=n).sum();
+    sum * sum
+}
+pub fn sum_of_squares(n: u32) -> u32 {
+    (1..=n).map(|x| x*x).sum()
+}
+
+pub fn difference(n: u32) -> u32 {
+    square_of_sum(n) - sum_of_squares(n)
+}
---
+++ rust/difference-of-squares/src/main.rs
@@ -0,0 +1,5 @@
+use difference_of_squares as squares;
+
+fn main() {
+    squares::square_of_sum(1);
+}
